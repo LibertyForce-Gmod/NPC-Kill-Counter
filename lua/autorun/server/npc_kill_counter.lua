6ff9d70dc6df10acbdde68c23f69481e0fdf7c96
LibertyForce-Gmod <ad89mail-githublifo@yahoo.com>
2015-11-01 18:02:50 +0100

    1.1
    Small code improvements.
    Replacing outdated functions.

--- lua/autorun/server/npc_kill_counter.lua
+++ lua/autorun/server/npc_kill_counter.lua
@@ -1,7 +1,7 @@
-local function NPCKillCounter(victim, killer, weapon)
-	if GetConVarNumber("npc_kill_counter_enabled") == 1 and IsValid(killer) then
+local function NPCKillCounter( victim, killer, weapon )
+	if GetConVar("npc_kill_counter_enabled"):GetBool() and IsValid( killer ) then
 		-- Only in SinglePlayer: If Indirect Count is enabled, give "Player 1" points for all deaths but those caused by NPCs.
-		if game.SinglePlayer() and not killer:IsNPC() and not killer:IsPlayer() and GetConVarNumber("npc_kill_counter_indirect") == 1 then
+		if game.SinglePlayer() and not killer:IsNPC() and not killer:IsPlayer() and GetConVar("npc_kill_counter_indirect"):GetBool() then
 			player.GetByID(1):AddFrags(1)
 		-- Gives direct kills to the killer.
 		elseif killer:IsPlayer() then
@@ -10,7 +10,7 @@ local function NPCKillCounter(victim, killer, weapon)
 	end
 end
 
+hook.Add("OnNPCKilled","NPCKillCounter", NPCKillCounter)
+
 CreateConVar( "npc_kill_counter_enabled", 1, FCVAR_ARCHIVE )
 CreateConVar( "npc_kill_counter_indirect", 0, FCVAR_ARCHIVE )
-
-hook.Add("OnNPCKilled","NPCKillCounter", NPCKillCounter)
